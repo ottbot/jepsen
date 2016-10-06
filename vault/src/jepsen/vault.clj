82fd4da0df6f3cb0d229c2aeed85b133ab44cbb5
Robert Crim <rob@servermilk.com>
2016-10-05 17:02:13 -0700

    WIP: set up boxes

--- vault/src/jepsen/vault.clj
+++ vault/src/jepsen/vault.clj
@@ -4,21 +4,53 @@
             [jepsen.db :as db]
             [jepsen.control :as c]
             [jepsen.tests :as tests]
-            [jepsen.os.debian :as debian]))
+            [jepsen.os.debian :as debian]
+            [clojure.java.io :as io]))
 
+
+;; Start consul
+;; consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul
+
+(def exec-task
+  (partial c/exec "/tmp/jepsen-db-tasks"))
+
+
+(defn node-ip-addresses [test])
 
 (defn db
-  "Vault"
+  "Vault transit"
   [version]
   (reify db/DB
     (setup! [_ test node]
-      (log/info node "installing Vault" version))
+
+      (c/su
+        (c/exec :echo
+                (str
+                  (slurp (io/resource "tasks")))
+                :> "/tmp/jepsen-db-tasks")
+
+        (c/exec :chmod :+x "/tmp/jepsen-db-tasks")
+
+        (log/info node "installing Vault" version)
+        (exec-task :install-vault version)
+
+        (log/info node "starting Consul")
+        (exec-task :install-consul)
+
+        (log/info node (:nodes test))
+        (exec-task :start-consul (count (:nodes test)))
+        ;; when node is :n1, join the servers. Looks up hostnames
+        ))
+
     (teardown! [_ test node]
+      (log/info node "trearing down Consul")
+      (c/su
+        (exec-task :stop-consul))
       (log/info node "tearing down Vault"))))
 
 (defn vault-test
   [version]
   (assoc tests/noop-test
-         :ssh {:username "admin"}                           ;; TODO -- change test nodes to allow root
+         :ssh {:username "admin"}                           ;; TODO -- change test nodes to allow root..
          :os debian/os
          :db (db version)))
